243bc8bc30b00d36f473c2fde969f30a2fdd3519
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-09-23 20:29:43 +0300

    react hooks children

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,20 +1,17 @@
 import React from "react";
 
-function Container({
-  header,
-  children,
-  textPosition = "", // possible values: left, right, center
-  direction = "horizontal", // possible values: vertical, horizontal
-  contentPosition = "center", // possible values: left, right, center
-}) {
+function Container(props){
+//   header,
+//   children,
+//   textPosition = "", // possible values: left, right, center
+//   direction = "horizontal", // possible values: vertical, horizontal
+//   contentPosition = "center", // possible values: left, right, center
+// }) {
   return (
-    <div className={`container ${textPosition}`}>
-      {header ? <h2>{header}</h2> : null}
-
-      <div className={`container-children ${direction} ${contentPosition}`}>
-        {children}
-      </div>
-    </div>
+    <>
+    <div className= {props.header}>{props.header}</div>
+    <div className= {props.children}>{props.children} </div>
+    </>
   );
 }
 
